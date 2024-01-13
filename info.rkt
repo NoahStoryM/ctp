84a867387c8bed10704d85f18d668e2d0eb4e058
NoahStoryM <noahstorym@gmail.com>
2024-01-13 11:01:55 +0800

    update `info.rkt`.

--- info.rkt
+++ info.rkt
@@ -3,11 +3,12 @@
 (define license 'MIT)
 (define version "0.0")
 (define collection "ctp")
-(define deps '("base"))
+(define deps '("base" "typed-racket-lib"))
 (define build-deps
   '("scribble-lib"
     "racket-doc"
+    "typed-racket-doc"
     "math-lib"
     "math-doc"))
-(define clean '("compiled"))
+(define clean '("compiled" "private/compiled"))
 (define scribblings '(("scribblings/ctp.scrbl" (multi-page) (getting-started))))
