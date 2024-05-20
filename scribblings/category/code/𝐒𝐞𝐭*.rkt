52ccc2236e5659f655dedd8c08de4890b415ae34
NoahStoryM <noahstorym@gmail.com>
2024-05-20 22:07:46 +0800

    use `hash-equal` instead of `hash-equal-always`.

--- scribblings/category/code/𝐒𝐞𝐭*.rkt
+++ scribblings/category/code/𝐒𝐞𝐭*.rkt
@@ -1,33 +1,33 @@
 #lang racket/base
 
 ;; Category of Pointed Sets
-(define (dom m) (make-immutable-hashalw (map (λ (k) (cons k k)) (hash-keys   m))))
-(define (cod m) (make-immutable-hashalw (map (λ (v) (cons v v)) (hash-values m))))
+(define (dom m) (make-immutable-hash (map (λ (k) (cons k k)) (hash-keys   m))))
+(define (cod m) (make-immutable-hash (map (λ (v) (cons v v)) (hash-values m))))
 (define ∘
   (case-lambda
     [(m) m]
     [(m1 m2)
-     (for/hashalw ([(k2 v2) (in-hash m2)])
+     (for/hash ([(k2 v2) (in-hash m2)])
        (define v1 (hash-ref m1 v2))
        (values k2 v1))]
     [(m1 m2 . m*) (apply ∘ (∘ m1 m2) m*)]))
-(define (? m) (and (hash? m) (hash-equal-always? m) (eq? '* (hash-ref m '* #f))))
+(define (? m) (and (hash? m) (hash-equal? m) (eq? '* (hash-ref m '* #f))))
 (define =
   (case-lambda
     [(_) #t]
-    [(m1 m2) (equal-always? m1 m2)]
+    [(m1 m2) (equal? m1 m2)]
     [(m1 m2 . m*) (and (= m1 m2) (apply = m2 m*))]))
 
 ;; Objects
-(define a #hashalw([* . *] [  0  .   0 ] [  1  .   1 ] [  2  .   2 ]))
-(define b #hashalw([* . *] [ |0| .  |0|] [ |1| .  |1|] [ |2| .  |2|]))
-(define c #hashalw([* . *] [ "0" .  "0"] [ "1" .  "1"] [ "2" .  "2"]))
-(define d #hashalw([* . *] [#"0" . #"0"] [#"1" . #"1"] [#"2" . #"2"]))
+(define a #hash([* . *] [  0  .   0 ] [  1  .   1 ] [  2  .   2 ]))
+(define b #hash([* . *] [ |0| .  |0|] [ |1| .  |1|] [ |2| .  |2|]))
+(define c #hash([* . *] [ "0" .  "0"] [ "1" .  "1"] [ "2" .  "2"]))
+(define d #hash([* . *] [#"0" . #"0"] [#"1" . #"1"] [#"2" . #"2"]))
 
 ;; Morphisms
-(define f #hashalw([* . *] [  0  .  |0|] [  1  .  |1|] [  2  .  |2|]))
-(define g #hashalw([* . *] [ |0| .  "0"] [ |1| .  "1"] [ |2| .  "2"]))
-(define h #hashalw([* . *] [ "0" . #"0"] [ "1" . #"1"] [ "2" . #"2"]))
+(define f #hash([* . *] [  0  .  |0|] [  1  .  |1|] [  2  .  |2|]))
+(define g #hash([* . *] [ |0| .  "0"] [ |1| .  "1"] [ |2| .  "2"]))
+(define h #hash([* . *] [ "0" . #"0"] [ "1" . #"1"] [ "2" . #"2"]))
 
 ;; Existence of composition
 (= b (cod f) (dom g))
