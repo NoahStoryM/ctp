3901907c62e05081a5165843c9f701915c1719a9
NoahStoryM <noahstorym@gmail.com>
2024-08-06 19:06:02 +0800

    Update `FSM.svg`

--- scribblings/code/functor/FSM.rkt
+++ scribblings/code/functor/FSM.rkt
@@ -6,20 +6,20 @@
 (: #;𝐒𝐞𝐭  𝒮 𝐂𝐚𝐭) (define (𝒮 m) m) (define ∘𝒮 compose)
 (: #;𝐋𝐢𝐬𝐭 ℒ 𝐂𝐚𝐭) (define (ℒ m) m) (define ∘ℒ append)
 
-(: 𝒞 (Listof (List A S S)))
-(define 𝒞
-  '([∘𝒞   𝒞2 𝒞1]
-    [dom𝒞 𝒞1 𝒞0]
-    [cod𝒞 𝒞1 𝒞0]
-    [id𝒞  𝒞0 𝒞1]))
+(: ℳ (Listof (List A S S)))
+(define ℳ
+  '([#\x b s]
+    [#\x s s]
+    [#\y s b]
+    [#\y b b]))
 
-(: A 𝒮) (define-type A (∪ '∘𝒞 'dom𝒞 'cod𝒞 'id𝒞))
-(: S 𝒮) (define-type S (∪ '𝒞0 '𝒞1 '𝒞2))
-(: s0 S) (define s0 '𝒞2)
+(: A 𝒮) (define-type A (∪ #\x #\y))
+(: S 𝒮) (define-type S (∪ 's 'b))
+(: s0 S) (define s0 's)
 (: φ (→ (× A S) S))
 (define (φ a s)
   (or
-   (for/or ([i : (List A S S) (in-list 𝒞)])
+   (for/or ([i : (List A S S) (in-list ℳ)])
      (match i
        [`(,(? (curry eq? a))
           ,(? (curry eq? s))
@@ -38,28 +38,24 @@
     ['() s]
     [`(,a . ,w) (φ a (φ* w s))]))
 
+(: run (→ String S))
+(define (run str) (φ* (reverse (string->list str)) s0))
+
 (module+ test
-  (check-eq? '𝒞1 (φ* '(∘𝒞) s0))
-  (check-eq? '𝒞0 (φ* '(cod𝒞 ∘𝒞) s0))
-  (check-eq? '𝒞0 (φ* '(dom𝒞 ∘𝒞) s0))
-  (check-eq? '𝒞1 (φ* '(id𝒞 cod𝒞 ∘𝒞) s0))
-  (check-eq? '𝒞1 (φ* '(id𝒞 dom𝒞 ∘𝒞) s0)))
+  (check-eq? 'b (run "yy"))
+  (check-eq? 'b (run "xy"))
+  (check-eq? 's (run "yx"))
+  (check-eq? 's (run "xx")))
 
 (: ∗ ℒ) (define ∗ (∘ℒ))
 (: Fφ* (→ #;A* (→ℒ ∗ ∗) (→𝒮 S S)))
 (define Fφ* (curry φ*))
 
 (module+ test
-  (check-eq? '𝒞1 ((Fφ* '(∘𝒞)) s0))
-  (check-eq? '𝒞0 ((Fφ* '(cod𝒞 ∘𝒞)) s0))
-  (check-eq? '𝒞0 ((Fφ* '(dom𝒞 ∘𝒞)) s0))
-  (check-eq? '𝒞1 ((Fφ* '(id𝒞 cod𝒞 ∘𝒞)) s0))
-  (check-eq? '𝒞1 ((Fφ* '(id𝒞 dom𝒞 ∘𝒞)) s0)))
-
-(module+ test
-  (check-eq?
-   ((Fφ* (∘ℒ '(dom𝒞) '(∘𝒞))) s0)
-   ((∘𝒮 (Fφ* '(dom𝒞)) (Fφ* '(∘𝒞))) s0))
-  (check-eq?
-   ((Fφ* (∘ℒ '(id𝒞 dom𝒞) '(∘𝒞))) s0)
-   ((∘𝒮 (Fφ* '(id𝒞 dom𝒞)) (Fφ* '(∘𝒞))) s0)))
+  (for* ([i (in-list '("x" "y" "xx" "xy" "yx" "yy"))]
+         [j (in-list '("x" "y" "xx" "xy" "yx" "yy"))])
+    (define m (reverse (string->list i)))
+    (define n (reverse (string->list j)))
+    (check-eq?
+     ((Fφ* (∘ℒ m n)) s0)
+     ((∘𝒮 (Fφ* m) (Fφ* n)) s0))))
