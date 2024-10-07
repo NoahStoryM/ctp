dbf73fcf19731b44866c54b98bab7e8734caf22f
NoahStoryM <noahstorym@gmail.com>
2024-10-07 10:50:53 +0800

    Update

--- scribblings/code/natural transformation/𝐒𝐞𝐭⇒𝐑𝐞𝐥.rkt
+++ scribblings/code/natural transformation/𝐒𝐞𝐭⇒𝐑𝐞𝐥.rkt
@@ -76,11 +76,9 @@
   ;; Objects in 𝒮
   (: a 𝒮) (define a (function (lazy a) (lazy a) #hash([x0 . x0] [x1 . x1])))
   (: b 𝒮) (define b (function (lazy b) (lazy b) #hash([y0 . y0] [y1 . y1])))
-  (: c 𝒮) (define c (function (lazy c) (lazy c) #hash([z0 . z0] [z1 . z1])))
 
   ;; Morphisms in 𝒮
   (: f (→𝒮 a b)) (define f (function (lazy a) (lazy b) #hash([x0 . y0] [x1 . y0])))
-  (: g (→𝒮 b c)) (define g (function (lazy b) (lazy c) #hash([y0 . z0] [y1 . z0])))
 
   (define check-𝐒𝐞𝐭⇒𝐑𝐞𝐥 (check-ntf 𝐒𝐞𝐭 𝐑𝐞𝐥))
   (define check-F⇒G (check-𝐒𝐞𝐭⇒𝐑𝐞𝐥 F G))
