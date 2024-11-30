8a0fc33a53dc94a9de740325ef38c61054302a4a
NoahStoryM <noahstorym@gmail.com>
2024-11-30 16:26:00 +0800

    Update `transpose.rkt`

--- scribblings/code/functor/transpose.rkt
+++ scribblings/code/functor/transpose.rkt
@@ -13,16 +13,16 @@
   (define ∘ compose)
 
   ;; Objects
-  (define a (identity-matrix 1))
-  (define b (identity-matrix 2))
-  (define c (identity-matrix 3))
-  (define d (identity-matrix 4))
+  (: a ℳ) (define a (identity-matrix 1))
+  (: b ℳ) (define b (identity-matrix 2))
+  (: c ℳ) (define c (identity-matrix 3))
+  (: d ℳ) (define d (identity-matrix 4))
 
   ;; Morphisms
-  (define f (build-matrix 1 2 rand))
-  (define g (build-matrix 2 3 rand))
-  (define h (build-matrix 3 4 rand))
+  (: f (→ℳ a b)) (define f (build-matrix 2 1 rand))
+  (: g (→ℳ b c)) (define g (build-matrix 3 2 rand))
+  (: h (→ℳ c d)) (define h (build-matrix 4 3 rand))
 
   (define check-†𝐌𝐚𝐭𝐫→𝐌𝐚𝐭𝐫 (check-ftr (∘ † 𝐌𝐚𝐭𝐫) 𝐌𝐚𝐭𝐫))
   (define check-T (check-†𝐌𝐚𝐭𝐫→𝐌𝐚𝐭𝐫 T))
-  (check-T a b c f g))
+  (check-T c b a g f))
